524b2420aa05a3fabcf6a764f25df373845706fb
marcelo dos santos silva <marcelo.iron09@gmail.com>
2017-02-22 17:14:24 -0300

    Create a project of ionic with angular 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  teste : string = "teste";
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
